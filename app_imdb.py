5bcb178aed90143b1e6a11dbfb60e3bf3e790b04
Satrouag <satyam15092001@gmail.com>
2025-02-20 11:41:54 +0530

    added the probability percentage feature, and displayed it in the frontend

--- app_imdb.py
+++ app_imdb.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
+import numpy as np
 
 # Load the trained model
 model = joblib.load("sentiment_model.pkl")
@@ -25,7 +26,18 @@ def predict():
         prediction = model.predict([user_text])  # Model expects a list
         sentiment = "Positive" if prediction[0] == 1 else "Negative"
 
-        return jsonify({"sentiment": sentiment})  # Send response to frontend
+        probabilities = model.predict_proba([user_text])[0]  # Get probabilities
+        # sentiment = "Positive" if np.argmax(probabilities) == 1 else "Negative"
+
+        pos_prob = round(probabilities[1] * 100, 2)  # Probability of positive sentiment
+        neg_prob = round(probabilities[0] * 100, 2)  # Probability of negative sentiment
+
+
+        return jsonify({
+            "sentiment": sentiment,
+            "positive_probability": pos_prob,
+            "negative_probability": neg_prob
+        })  # Send response to frontend
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
